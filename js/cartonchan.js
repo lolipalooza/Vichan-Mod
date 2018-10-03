0396e271e96a08e192ef147243071716b6dd838a
mumifaglalolanda <mumifag_lalolanda@hotmail.com>
2018-10-03 19:50:23 -0400

    Mejorando el código de los juguetes de email

--- js/cartonchan.js
+++ js/cartonchan.js
@@ -4,13 +4,13 @@
  */
  
 onready(function () {
-	$("#email-sel").on( "change", function() {
-		
-		var emailFunction = $("#email-sel option:selected").val();
-		
-		if (emailFunction != "" && emailFunction != "sage" && emailFunction != "noko") {
-			emailFunction = "#" + emailFunction;
-		}
+	$(".email-sel").on( "change", function() {
+		var emailFunction = $(".email-sel option:selected").val();
 		$("input[name=email]").val( emailFunction );
 	});
+	
+	$("input[name=email]").bind( 'input', asdf = function() {
+		var sel = $("input[name=email]").val();
+		$(".email-sel").val( sel );
+	}).on( 'change', asdf );
 });
